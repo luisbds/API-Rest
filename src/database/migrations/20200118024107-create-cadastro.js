b1e4a606c802835ad863c2f417c5c92e28b5a92d
luisbds <duduluis559@hotmail.com>
2020-01-18 00:20:38 -0300

    up

---
+++ src/database/migrations/20200118024107-create-cadastro.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    //dizer o que a migrations vai realizar na db
+    return queryInterface.createTable('cadastro', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      senha: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+    })
+    
+  },
+
+  down: (queryInterface, Sequelize) => {
+   //se der alguma coisa errada o que tenho que fazer
+   return queryInterface.dropTable('cadastro') 
+  }
+};
